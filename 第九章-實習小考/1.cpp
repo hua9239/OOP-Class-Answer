402aa213a642b94f1f5f96633a69b20d0f594e08
Likai <B2202846@ulive.pccu.edu.tw>
2024-03-10 18:00:13 +0800

    upload chapter 9 - exam - 1

---
+++ 第九章-實習小考/1.cpp
@@ -0,0 +1,21 @@
+/*
+請寫出一個物件StuCard，該物件包含下列屬性 : string name(學生姓名)、
+string stdID(學號)、double chinese(國文成績) double english(英文成績)、
+double math(數學成績)、double science(自然成績)、double history(歷史成績)、
+double geography(地理成績)、double computerScience(計算機概論成績)
+
+輸入說明: 輸入兩組 學生姓名 string、學號 string、國文成績 double、
+英文成績 double、數學成績 double、自然成績 double、歷史成績 double、地理成績 double、計算機概論成績 double
+
+輸出說明: 輸出兩組物件的姓名、學號、七個課目的成績取平均(取至小數點第一位)
+
+範例輸入:
+
+Charlie B1234567 85 90 95 90 80 75 100
+Evan B7654321 70 55 45 60 70 90 50
+
+範例輸出:
+
+Charlie B1234567 87.9
+Evan B7654321 62.9
+*/
